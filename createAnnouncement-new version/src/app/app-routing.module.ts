05a949706339d2be6ae798d12d0a3301187740fb
QuinnMarshal <marshallq4711@gmail.com>
2022-07-25 15:38:06 -0700

    added login function -Quinn

--- createAnnouncement-new version/src/app/app-routing.module.ts
+++ createAnnouncement-new version/src/app/app-routing.module.ts
@@ -5,13 +5,20 @@ import { DiscussionComponent } from './discussion/discussion.component';
 import { AssignmentComponent } from './assignment/assignment.component';
 import { QuizzesComponent } from './quizzes/quizzes.component';
 import { ModulesComponent } from './modules/modules.component';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+
+
 
 const routes: Routes = [
   {path: 'announcements', component:AnnouncementsComponent},
   {path: 'discussion', component:DiscussionComponent},
   {path: 'assignment', component:AssignmentComponent},
   {path: 'quizzes', component:QuizzesComponent},
-  {path: 'modules', component:ModulesComponent}
+  {path: 'modules', component:ModulesComponent},
+  {path: 'main', component:MainComponent},
+  {path: 'login', component:LoginComponent}
+
 ];
 
 @NgModule({
@@ -19,4 +26,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [AnnouncementsComponent, DiscussionComponent, AssignmentComponent,QuizzesComponent,ModulesComponent];
+export const routingComponents = [AnnouncementsComponent, DiscussionComponent, AssignmentComponent,QuizzesComponent,ModulesComponent,MainComponent,LoginComponent];
